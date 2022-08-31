1f313dea96c8e4ef7630493deb30136b83b2e10f
LiubovSokolova89 <liubow.sokolova@gmail.com>
2022-08-31 22:41:25 +0200

    Initial commit

---
+++ src/main/java/ru/netology/manager/Manager.java
@@ -0,0 +1,51 @@
+package ru.netology.manager;
+
+import ru.netology.product.Product;
+import ru.netology.repository.Repository;
+
+public class Manager {
+    protected Repository repo;
+
+    public Manager(Repository repo) {
+        this.repo = repo;
+    }
+
+
+    public void add(Product product) {
+        repo.GetAllSavedProducts(product);
+    }
+
+    public Product[] findAll() {
+        return repo.findAll();
+    }
+
+
+        public Product[] searchBy(String text) {
+            Product[] result = new Product[0];
+            for (Product product: repo.findAll()) {
+                if (matches(product, text)) {
+                    Product[] tmp = new Product[result.length + 1];
+                    for(int i = 0; i < result.length; i++) {
+                        tmp[i] = result[i];
+                    }
+                    tmp[tmp.length -1] = product;
+                    result = tmp;
+                }
+            }
+            return result;
+        }
+
+        public boolean matches(Product product, String search) {
+            if (product.getName().contains(search)) {
+                return true;
+            } else {
+                return false;
+            }
+            // или в одну строку:
+            // return product.getName().contains(search);
+        }
+    }
+
+
+
+
---
+++ src/test/java/ru/netology/manager/ManagerTest.java
@@ -0,0 +1,68 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.book.Book;
+import ru.netology.product.Product;
+import ru.netology.repository.Repository;
+import ru.netology.smartphone.Smartphone;
+
+public class ManagerTest {
+
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+
+        Product Book1 = new Book(123, "Скала" , 500, "Шевченко");
+        Product Book2 = new Book(657, "Лето на озере" , 456, "Шевченко");
+        Product Book3 = new Book(483, "Марс" , 674, "Коваль");
+        Product Smartphone1 = new Smartphone(8954, "Orange 10" , 56000, "Orange");
+        Product Smartphone2 = new Smartphone(47565, "Horse 11" , 34000, "Horse");
+        Product Smartphone3 = new Smartphone(347457, "Horse 9" , 25000, "Horse");
+
+
+    @Test
+    public void ShowAddByName() {
+
+        manager.add(Book1);
+        manager.add(Book2);
+        manager.add(Book3);
+        manager.add(Smartphone1);
+        manager.add(Smartphone2);
+        manager.add(Smartphone3);
+
+        Product[] actual = manager.searchBy( "Horse");
+        Product[] expected = {Smartphone2, Smartphone3};
+
+        Assertions.assertArrayEquals(expected , actual);
+
+
+    }
+
+    @Test
+    public void ShouldFindNothingIfTheManagerIsEmpty() {
+         Product[] actual = manager.searchBy("Horse");
+         Product[] expected = {};
+    }
+
+//    @Test
+//    public void ShouldRemoveById() {
+
+//        repo.GetAllSavedProducts(Book1);
+//        repo.GetAllSavedProducts(Book2);
+//        repo.GetAllSavedProducts(Book3);
+//        repo.GetAllSavedProducts(Smartphone1);
+//        repo.GetAllSavedProducts(Smartphone2);
+//        repo.GetAllSavedProducts(Smartphone3);
+
+//        repo.removeById(Book1.getId());
+
+//        Product[] actual = repo.
+//        Product[] expected = {Book2, Book3, Smartphone1, Smartphone2, Smartphone3};
+
+
+//    }
+
+
+
+
+}
